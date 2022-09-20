35cd7549de57cea499015fdf0caf0eac85dbee7c
rendiwidi <rendi.widi@gmail.com>
2022-09-20 23:03:28 +0700

    update testimonials pages

--- pages/Testimonials.vue
+++ pages/Testimonials.vue
@@ -13,7 +13,7 @@ useHead({
   <section>
     <div class="container flex flex-col px-5 py-10 mx-auto md:flex-row">
       <div
-        class="flex flex-col py-5 text-center lg:flex-grow md:w-1/2 lg:px-24 md:items-start md:text-left"
+        class="flex flex-col py-5 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:items-start md:text-left"
       >
         <h2 class="mb-4 text-lg font-bold text-gray-600">
           BECOME A LEGENDARY DESIGNER
@@ -83,14 +83,17 @@ useHead({
               <div
                 class="flex items-center p-4 bg-white rounded-lg drop-shadow-lg hover:bg-gray-200"
               >
-              <img alt="Blue Internet icon" srcset="https://img.icons8.com/ultraviolet/2x/internet--v1.png 2x">
+                <img
+                  alt="Blue Internet icon"
+                  srcset="
+                    https://img.icons8.com/ultraviolet/2x/internet--v1.png 2x
+                  "
+                />
 
                 <div class="mx-auto">
                   <h1 class="font-bold md:text-2xl">20+</h1>
                   <p class="text-sm text-gray-500">Country served</p>
                 </div>
-
-              
               </div>
             </div>
           </div>
@@ -103,6 +106,55 @@ useHead({
           alt="hero"
           src="https://images.unsplash.com/photo-1493020258366-be3ead1b3027?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d29yayUyMGZpbmFuY2V8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         />
+      </div>
+    </div>
+
+    <!-- Slide 2 -->
+
+    <div
+      class="container flex flex-col py-10 pl-5 mx-auto lg:flex-grow md:flex-row"
+    >
+      <div class="w-full h-auto mb-10 md:mb-0">
+        <div class="w-auto h-full">
+          <img
+            class="w-full h-auto rounded"
+            alt="hero"
+            src="https://images.unsplash.com/photo-1571844307880-751c6d86f3f3?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29tZW4lMjBidXNpbmVzc3xlbnwwfDB8MHx8&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
+          />
+        </div>
+      </div>
+
+      <div class="w-full h-auto mb-10 md:mb-0">
+        <div class="w-auto h-full pl-24">
+          <p
+            class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-xl"
+          >
+            <span class="block xl:inline"
+              >At Designership, they ship truly brilliant products! It’s easy to
+              use and scale, and is really handy to customise for any projects.
+            </span>
+          </p>
+          <p
+            class="mx-auto mb-24 text-base text-neutral-500 sm:max-w-md md:max-w-3xl lg:text-lg"
+          >
+            Ac faucibus orci id quis consectetur laoreet sed. Enim congue
+            molestie nam odio pulvinar ac ultricies. Elementum ut pellentesque
+            volutpat mi. Faucibus sit posuere nisi aenean ultrices. Suscipit
+            malesuada aenean nullam et sit dapibus eget a eu. Nisl tortor arcu
+            et.
+          </p>
+
+          <p
+            class="mx-auto text-base font-bold text-neutral-900 sm:max-w-md md:max-w-3xl lg:text-l"
+          >
+            PROFEED CEO
+          </p>
+          <p
+            class="mx-auto text-base font-bold text-neutral-500 sm:max-w-md md:max-w-3xl lg:text-sm"
+          >
+            Founder, Benefeed & Profeed (BTC)
+          </p>
+        </div>
       </div>
     </div>
   </section>
